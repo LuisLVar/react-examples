cdf097e47c87dfc6ceaf24097f066015eebeead0
Luis Angel Vargas Leon <LuisVargas@MacBook-LuisVargas.local>
2022-06-09 20:32:27 -0600

    reducer con localstorage

--- contador/src/Components/TodosReducer/reducer.js
+++ contador/src/Components/TodosReducer/reducer.js
@@ -1,13 +1,17 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case 'CHECK':
-      return state.map((todo) => {
+      let newState =  state.map((todo) => {
         if(todo.id === action.id){
           return {...todo, complete: !todo.complete};
         }else{
           return todo;
         }
       })
+      localStorage.setItem('state', JSON.stringify(newState));
+      let localStorageItem = JSON.parse(localStorage.getItem('state'));
+      console.log(localStorageItem)
+      return newState;
     default:
       return state;
   }
